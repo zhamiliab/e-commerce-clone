16727cbaab4e43c2147c4ef2c821ad3638200668
zhamiliabegalieva <jbegalieva@gmail.com>
2021-12-30 21:21:01 -0800

    created repo styling the mainPage img

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-import HomePage from './homepage.component';
+import HomePage from './pages/homeplage/homepage.component';
 
 import './App.css';
 
 class App extends React.Component {
-  render() {
-    return (
-      <div className='App'>
-        <HomePage />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<HomePage />
+			</div>
+		);
+	}
 }
 
 export default App;
